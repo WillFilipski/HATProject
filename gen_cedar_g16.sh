3c7c7fa847e56c6cdb322f7c7d967e3a7cf56ca2
Will F <48337016+WillFilipski@users.noreply.github.com>
2021-06-18 10:52:37 -0700

    Update gen_cedar_g16.sh
    UPDATE: wall time to 1 day
    UPDATE: account=ctb-dilabiog -> account=def-dilabiog-ac

--- gen_cedar_g16.sh
+++ gen_cedar_g16.sh
@@ -5,14 +5,14 @@ for ii in *.gjf ; do
 
     cat > ${i}.sub <<EOF
 #! /bin/bash
-#SBATCH -t 28-00:00
+#SBATCH -t 1-00:00
 #SBATCH -J ${i}
 #SBATCH -o ${i}.out
 #SBATCH -e ${i}.err
 #SBATCH -N 1
 #SBATCH -n 32
 #SBATCH --mem-per-cpu=4000M
-#SBATCH --account=ctb-dilabiog
+#SBATCH --account=def-dilabiog-ac
 
 module load gaussian
 
