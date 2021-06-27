f428439ee9d52a154819314260e77b22306492e3
Will F <48337016+WillFilipski@users.noreply.github.com>
2021-06-26 22:36:55 -0700

    Update gen_sub_xyz.sh

--- gen_sub_xyz.sh
+++ gen_sub_xyz.sh
@@ -14,16 +14,50 @@ H3C...H...OH  TS calculation
 
 0 2
 EOF
-    LINE_COUNT=1
     while read -r LINE ; do
-            ((LINE_COUNT++))
-
-            if [[ $LINE == "Bonds" ]] ; then
+            if [[ "$LINE" =~ "Bonds" ]] ; then
                 break
             else
                 echo "$LINE" >> "${i}.gjf"
             fi
     done < "./${i}.xyz"
 
-    echo "READ: ${i}.xyz | WRITE: ${i}.gjf"
+    echo "READ: ${i}.xyz"
 done
+    
+for ii in *.xyz; do
+    #ITERATE FILES
+    i=${ii%.xyz}
+
+    read -p "WRITE ${i}.gjf | Bonds to freeze:" B1 B2 B3
+    echo -e "\nB $B1 $B2 F">> "${i}.gjf"
+    echo -e "B $B2 $B3 F\n">> "${i}.gjf"
+
+    cat >> ${i}.gjf <<EOF
+--Link1--
+%chk=${i}.chk
+%MEM=32GB
+%nproc=8
+#M062x 6-31G* Geom=(checkpoint,modredundant) Guess=Read Freq
+
+H3C...H...OH  TS calculation
+
+0 2
+
+B $B1 $B2 A
+B $B2 $B3 A
+
+--Link1--
+%chk=${i}.chk
+%MEM=32GB
+%nproc=8
+#M062x 6-31G* Geom=CheckPoint Guess=Read OPT=(TS,ReadFC,NoEigenTest) Freq
+
+H3C...H...OH  TS calculation
+
+0 2
+
+EOF
+done
+
+bash ./gen_cedar_g16.sh
