1ac3a2978d23af2f13f3f618081a15542545413c
Will F <48337016+WillFilipski@users.noreply.github.com>
2021-07-08 00:22:34 -0700

    Update gen_sub_xyz.sh
    ADDED: Case statement for job submission to queue.

--- gen_sub_xyz.sh
+++ gen_sub_xyz.sh
@@ -66,7 +66,21 @@ echo "Successfully transposed bond information."
 echo "Executing ./gen_cedar_g16.sh"
 bash ./gen_cedar_g16.sh
 
-echo "Submitting jobs..."
-for i in *.sub ; do sbatch $i ; done
+echo "Submit jobs to queue? Y/N"
+read ANS
+case $ANS in
+    [Yy])
+    echo "Submitting jobs..."
+    for i in *.sub ; do sbatch $i ; done
+    ;;
+
+    [Nn])
+    echo "Quitting program..."
+    ;;
+
+    *)
+    echo "Invalid input."
+    ;;
+esac
 
 echo "Done"
